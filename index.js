5cc8eacb189f74945cd9fb2b920620f2dca3f013
Melanie <melican@LAPTOP-GKKKJDUA.>
2023-11-07 15:16:18 -0500

    latest

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeaderElement = document.createElement("h1");
+document.body.appendChild(newHeaderElement);
+newHeaderElement.id = "victory"; 
+const newHeader = document.getElementById('victory'); 
+
+newHeader.textContent = "Melican is the champion";
